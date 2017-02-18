4e191d83f1b1bb88a4ba5cfad6e4c90aa5cf2f2f
King Of Hello World <never_ls@foxmail.com>
2017-02-19 01:00:48 +0800

    Update word_practice.py

--- word_practice.py
+++ word_practice.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Author: levenls, i am
+# @Author: levenls, i am here
 # @Date:   2016-08-19 16:20:58
 # @Last Modified by:   leven-ls
 # @Last Modified time: 2016-08-20 16:32:11
